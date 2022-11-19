e019f63a617f4e6409e7c33b38d007535b28a9be
abdelbif <103958187+abdelbif@users.noreply.github.com>
2022-11-19 15:27:46 -0600

    xisAsDoI

---
+++ docs/W2D1-Lab6&7/js/lab6.js
@@ -0,0 +1,32 @@
+//Question 1
+
+//Details on picture
+//undefined
+//8
+//8
+//9
+//10
+//1
+
+//Question 2
+
+// Global scope is the environment that exits outside of any function.
+// Local scope is the environment within o block or a function.
+
+// Question 3
+//a. No
+//b. Yes
+//c. No
+//d. Yes
+//e. Yes
+
+//Question 4
+
+//81
+//25
+//Details on picture
+
+//Question 5
+
+//10
+//Details on picture
